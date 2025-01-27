671cb6673c57ec6f6c6ddebd527f717784b6a6a6
andrronov <andrew270504cska@gmail.com>
2025-01-27 16:03:47 +0500

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+#My custom templates for Vite
---
+++ vue-ts-tcss/src/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="home">
+        <h1>Home</h1>
+        <p>
+        Welcome to your new project! This is the home page.
+        </p>
+    </div>
+</template>
